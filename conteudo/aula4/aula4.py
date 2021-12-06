0a6810cf1d7a5c5f1985cb0f4079c4b9be3378d6
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-06 18:06:06 -0300

    commit casas decimais

--- conteudo/aula4/aula4.py
+++ conteudo/aula4/aula4.py
@@ -3,7 +3,7 @@ Tipos de Dados:
 
 str (string): textos 'assim' "assim"
 int (inteiro): 123456 -10 -20
-float (real/ponto flutuante): 10.50 ; 1.5 ; -10.53
+float (real/ponto flutuante/casas decimais): 10.50 ; 1.5 ; -10.53
 bool (booleano/lógico): true / false
 """
 
