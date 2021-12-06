e3540f3dcb0aed2183da47e8364d93f0bbeb87c9
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-06 17:48:32 -0300

    commit aula 3

--- conteudo/aula2/aula2.py
+++ conteudo/aula2/aula2.py
@@ -1,1 +1,8 @@
-# O comando print
+# O comando print imprime na tela o que é desejado. Aqui veremos o conceito de separação e end.
+"""
+Ex.: tenho o seguinte CPF (gerado aleatoriamente) e gostaria de separa-lo com ponto e traço, como está abaixo:
+824.176.070-18
+"""
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
---
+++ conteudo/aula3/aula3.py
@@ -0,0 +1,7 @@
+#Estudos sobre strings. Strings são textos que estão entre aspas.
+
+"""
+str - string
+"""
+print('Essa é uma string (str).')
+print(r'Esse é meu \n.') #com o R no início, podemos inserir o código sem ser executado.
