bbae55e03675ea6802a97bf4a8f27187cb61a425
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-06 17:17:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# curso-python
+ Estudos sobre Python.
---
+++ aula01/aula01.py
@@ -0,0 +1,4 @@
+# Comentários em Python são feitos com o auxílio de # ou """
+
+# Print('hello world') o que está comentado, não será executado
+print('hello world')
