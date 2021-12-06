919705793309c58dcf3705401852c3e29057f586
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-06 17:51:57 -0300

    commit tipo de dados

---
+++ conteudo/aula4/aula4.py
@@ -0,0 +1,8 @@
+"""
+Tipos de Dados:
+
+str (string): textos 'assim' "assim"
+int (inteiro): 123456 -10 -20
+float (real/ponto flutuante): 10.50 ; 1.5 ; -10.53
+bool (booleano/lógico): true / false
+"""
