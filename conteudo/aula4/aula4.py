3c9302278bd7aa861e2fb85c7f1dac81b3e0956b
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-06 18:04:35 -0300

    commit types

--- conteudo/aula4/aula4.py
+++ conteudo/aula4/aula4.py
@@ -6,3 +6,8 @@ int (inteiro): 123456 -10 -20
 float (real/ponto flutuante): 10.50 ; 1.5 ; -10.53
 bool (booleano/lógico): true / false
 """
+
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
