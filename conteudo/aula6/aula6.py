b2e535e511169a5963c4d0051b1c104bd2c8283e
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-13 17:10:33 -0300

    aula6 - variaveis

---
+++ conteudo/aula6/aula6.py
@@ -0,0 +1,16 @@
+"""
+VARIÁVEIS:
+Iniciam com letra, podem conter números, separar _, letras minúsculas.
+"""
+
+nome = 'Vitória'
+idade = 18
+altura = 1,75
+e_maior = idade >= 18
+data_1 = True
+data_atual = 2021
+
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('É maior: ', e_maior)
