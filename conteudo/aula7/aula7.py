53de9892705e2174b225c3cb2ac8c1d1b184a155
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-13 17:16:58 -0300

    aula 7 - formatando strings

---
+++ conteudo/aula7/aula7.py
@@ -0,0 +1,10 @@
+# FORMATANDO STRINGS
+
+nome = 'Vitória'
+idade = 18 # int
+altura = 1.75 #float
+e_maior = idade >= 18 #bool
+peso = 57
+imc = peso / (altura ** 2)
+
+print(f'{nome} tem {idade} anos e seu imc é {imc:.2f}') 
