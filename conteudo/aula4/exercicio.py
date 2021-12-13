6691d15c24031eed568bada1548df84766edfb77
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-13 17:02:48 -0300

    aula 5

---
+++ conteudo/aula4/exercicio.py
@@ -0,0 +1,11 @@
+# String: nome
+print('Vitoria', type('Vitória'))
+
+# Idade: int
+print(18, type(18))
+
+# Altura: float
+print(1.65, type(1.65))
+
+# Boolean: é maior de idade
+print (18 > 18, type(18 > 18))
---
+++ conteudo/aula5/aula5.py
@@ -0,0 +1,21 @@
+# OPERADORES ARITMÉTICOS
+
+"""
+Os operadores aritméticos são: 
+soma +
+subtração -
+multiplicação *
+divisão flutuante /
+divisão inteira // 
+potenciação ** 
+retorna módulo (resto de divisão) % 
+alterar a precedência ()
+"""
+
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+print(10 // 10)
+print(10 ** 10)
+print(10 % 10)
