c5b3e9e085b10befdf3d4a38f89e90def388748b
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-13 17:38:27 -0300

    aula 11 - operadores relacionais

---
+++ conteudo/aula11/aula11.py
@@ -0,0 +1,19 @@
+"""
+Operadores Relacionais
+== igualdade
+> maior que
+>= maior ou igual que
+< menor que 
+<= menor ou igual que
+!= diferente
+"""
+
+num1 = 10
+num2 = 5
+
+print(num1 == num2)
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 != num2)
