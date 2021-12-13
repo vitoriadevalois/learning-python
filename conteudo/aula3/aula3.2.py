2b1acf3e37d5a77c4dfa901f1f7e9227e5f36e14
vitoriadevalois <vitoriadvalois@gmail.com>
2021-12-13 17:29:49 -0300

    aula 3.2 - entrada de dados

---
+++ conteudo/aula3/aula3.2.py
@@ -0,0 +1,11 @@
+"""
+Entrada de dados - Aula 3
+"""
+
+nome = input('Qual o seu nome? ')
+print(f'O usuário digitou {nome} e o tipo da variável é ', f'{type(nome)}.')
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite um número: '))
+
+print(num1 + num2)
